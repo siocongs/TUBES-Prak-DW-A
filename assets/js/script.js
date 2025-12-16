db22a49e156903a67b0e8672b53d60e39969a172
Si Ocong <81248499+siocongs@users.noreply.github.com>
2025-12-16 14:24:11 +0700

    bachtiar: Add Event Listener current user for replace right menu nav class, add function logout

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,27 @@ window.addEventListener("scroll", () => {
 
   lastScroll = currentScroll;
 });
-
-
 // End NAVBAR SCROLL
+
+// LOGIN SESION
+document.addEventListener("DOMContentLoaded", () => {
+  const user = JSON.parse(localStorage.getItem("currentUser"));
+
+  if (user) {
+      const authArea = document.querySelector(".d-flex.align-items-center.gap-3");
+      if (authArea) {
+          authArea.innerHTML = `
+              <a href="store.html" class="text-dark text-decoration-none">
+                  <i class="bi bi-geo-alt"></i> Find a store
+              </a>
+              <span class="fw-semibold">Hi, ${user.firstName}</span>
+              <button class="btn btn-outline-dark rounded-pill px-3" onclick="logout()">Logout</button>
+          `;
+      }
+  }
+});
+
+function logout() {
+  localStorage.removeItem("currentUser");
+  location.reload();
+}
